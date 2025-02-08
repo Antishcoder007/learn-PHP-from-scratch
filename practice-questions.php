92702f20b94c39b55033dd0cec26d8ee0c889613
Antish kumar <96709262+Antishcoder007@users.noreply.github.com>
2025-02-08 18:34:09 +0530

    Update practice-questions.php

--- practice-questions.php
+++ practice-questions.php
@@ -1,11 +1,17 @@
-# Day1
-there are no practical question for day1
----
+Day1: there are no practical questions for day1
+Day 2
+✅ **Practice Questions**
+Q1 Declare a variable $city and assign your city name. Print it.
+Ans:
+  <?PHP
+    $city = Jaipur;
+    echo "city is ".$city;
+  ?>
 
-# Day 2
-## ✅ **Practice Questions**
-### Q1 Declare a variable $city and assign your city name. Print it.
-### Q2 Store an integer, string, and float in variables and display them.
-### Q3 Define a constant WEBSITE with your website name and print it.
-### Q4 Create an array of your favorite movies and print each using echo.
-### Q5 Write a multi-line comment explaining what your PHP script does.
+output: city is jaipur
+
+
+Q2 Store an integer, string, and float in variables and display them.
+Q3 Define a constant WEBSITE with your website name and print it.
+Q4 Create an array of your favourite movies and print each using Echo.
+Q5 Write a multi-line comment explaining what your PHP script does.
