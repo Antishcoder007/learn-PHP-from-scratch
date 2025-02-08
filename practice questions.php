462e05bf1898ee621c7b31d6b653b9aa051a0909
Antish kumar <96709262+Antishcoder007@users.noreply.github.com>
2025-02-08 18:12:42 +0530

    Update and rename practice questions to practice-questions.php

---
+++ practice questions.php
@@ -0,0 +1,11 @@
+# Day1
+there are no practical question for day1
+---
+
+# Day 2
+## ✅ **Practice Questions**
+### Q1 Declare a variable $city and assign your city name. Print it.
+### Q2 Store an integer, string, and float in variables and display them.
+### Q3 Define a constant WEBSITE with your website name and print it.
+### Q4 Create an array of your favorite movies and print each using echo.
+### Q5 Write a multi-line comment explaining what your PHP script does.
